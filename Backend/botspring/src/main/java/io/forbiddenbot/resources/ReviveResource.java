d74866fb8762c2bed2cf6c2f45a26a220c22e98e
Frodo <jvfaggion@hotmail.com>
2020-03-05 23:04:16 -0300

    Added revive function

---
+++ Backend/botspring/src/main/java/io/forbiddenbot/resources/ReviveResource.java
@@ -0,0 +1,16 @@
+package io.forbiddenbot.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/revive")
+public class ReviveResource {
+
+	@RequestMapping(method=RequestMethod.GET)
+	public String pulse() {
+		return "Monster reborned.";
+	}
+	
+}
--- Backend/botspring/src/main/java/io/forbiddenbot/services/JobScheduler.java
+++ Backend/botspring/src/main/java/io/forbiddenbot/services/JobScheduler.java
@@ -1,9 +1,15 @@
 package io.forbiddenbot.services;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 @Component
 @EnableScheduling
@@ -13,9 +19,30 @@ public class JobScheduler {
 	private ForbiddenService forbiddenService;
 
 	
-	@Scheduled(fixedRate=14400000)
+	@Scheduled(fixedRate=3600000)
     public void generateForbidden() {
 		forbiddenService.generateNewForbidden();
     }
+	
+private final OkHttpClient httpClient = new OkHttpClient();
+	
+	@Scheduled(fixedDelay = 1500000)
+	 public void monsterReborn() throws IOException {
+		
+
+		Request request = new Request.Builder()
+                .url("https://forbidden-bot.herokuapp.com/revive")
+                .build();
+
+        try (Response response = httpClient.newCall(request).execute()) {
+
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+            // Get response body
+            System.out.println(response.body().string());
+        }
+
+		
+   }
 	
 }
